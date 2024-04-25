6c51f22b7b80c421f9446256cd4bcee26bb55b68
Aysha Farheen <afarheen@lab3r2s4.42abudhabi.ae>
2024-04-25 09:34:16 +0400

    server functionality complete

--- client.cpp
+++ client.cpp
@@ -1,25 +1,6 @@
 #include "IRC.hpp"
 
-void Client::create_client()
-{
-	int client = socket(AF_INET, SOCK_STREAM, 0);
-	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(8080);
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
-	connect(client, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-	const char* message = "Hello, server!";
-	send(client, message, strlen(message), 0);
-	close(client);
-}
-
 Client::Client(int socket)
 {
 	fd = socket;
 }
-
-// int main(int argc, char **argv)
-// {
-
-// 		Client::create_client();
-// }
